c364618de332159d0b4f27989daa5ec326940a4e
Marc-Antoine <65295801+MaTillie@users.noreply.github.com>
2025-01-24 16:58:30 +0100

    Iinit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+name "gm-bridge"
+author "Gaïus Mancini"
+description "Bridge for ccript By Gaïus Mancini"
+fx_version "cerulean"
+game "gta5"
+dependencies {
+}
+shared_script 'config.lua'
+client_script 'client/*.lua'
+server_script 'server/*.lua'
+lua54 'yes'
